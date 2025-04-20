6e1342168a09c947b892eb21e0c9446a9370d8d8
MazenSokkar <mazensokar.priv@gmail.com>
2025-04-20 21:51:04 +0200

    Add initial model classes for FastFoodDelivery application including AppUser, Cart, Category, Item, and SubCategory. Also, update project file to include Microsoft.AspNetCore.Identity.EntityFrameworkCore package.

---
+++ FastFoodDelivary.Models/Cart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFoodDelivery.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        [Required, MinLength(1)]
+        public int Count { get; set; }
+    }
+}
